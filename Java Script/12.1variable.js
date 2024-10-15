5291e20446a736aacdef8a9ae0b577c6383b4f4a
FyslAhmd <info.faysal.32@gmail.com>
2024-10-15 19:43:12 +0600

    new js added

---
+++ Java Script/12.1variable.js
@@ -0,0 +1,37 @@
+var price = 120;
+console.log(price);
+
+var name = "Faysal Ahmed";
+console.log(name);
+
+var isHot = true;
+console.log(isHot);
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.indexOf('sal'));
+console.log(name.split('a'));
+
+var num1 = 10;
+var num2 = '10.5';
+num2 = parseFloat(num2);
+console.log(num1 + num2);
+
+var num3 = '100';
+num3 = +num3;
+console.log(num1+num3);
+
+num1 = ''+num1;
+console.log(typeof num1);
+
+var number1 = -5;
+var number2 = 1.20
+console.log(Math.abs(number1));
+console.log(Math.round(number2));
+console.log(Math.ceil(number2));
+console.log(Math.floor(number2));
+console.log(Math.floor(Math.random()*100));
+
+
+var date = new Date();
+console.log(date);
---
+++ Java Script/12.2condition.js
@@ -0,0 +1,10 @@
+let price = 10;
+
+if(price < 10) {
+    console.log("I will Eat.");
+} 
+else if(price = 10) {
+    console.log("Kisu naa.")
+} else {
+    console.log("I will not eat.");
+}
